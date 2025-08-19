1a4ad3c4055737f07b4cb46fcd3a1f14ddff660e
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2025-08-19 11:18:31 +0530

    Default generated codes.

---
+++ IdGenerators/src/main/java/com/rit/hibernate/IdGeneratorsApplication.java
@@ -0,0 +1,13 @@
+package com.rit.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdGeneratorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdGeneratorsApplication.class, args);
+	}
+
+}
---
+++ IdGenerators/src/test/java/com/rit/hibernate/IdGeneratorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rit.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdGeneratorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
