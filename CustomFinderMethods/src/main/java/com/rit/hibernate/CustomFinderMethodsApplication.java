898fe3ff5fecef04848be854f7e8d673cd68a28d
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2025-08-30 19:05:59 +0530

    Default/Generated code

---
+++ CustomFinderMethods/src/main/java/com/rit/hibernate/CustomFinderMethodsApplication.java
@@ -0,0 +1,14 @@
+package com.rit.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomFinderMethodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomFinderMethodsApplication.class, args);
+		System.out.println("Basic test");
+	}
+
+}
