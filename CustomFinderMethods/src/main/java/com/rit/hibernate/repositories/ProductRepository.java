42779d1f8395537d892e68f1ebf9cf6520fdb4da
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2025-09-04 07:45:13 +0530

    Not required to have JPARepository as of now.

--- CustomFinderMethods/src/main/java/com/rit/hibernate/repositories/ProductRepository.java
+++ CustomFinderMethods/src/main/java/com/rit/hibernate/repositories/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.rit.hibernate.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.rit.hibernate.entities.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends CrudRepository<Product, Long> {
 
 }
